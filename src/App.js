0472e76643a93cf1e684af34f65aac3f6038db0f
Singamsai <103039188+Singamsai@users.noreply.github.com>
2023-03-05 17:53:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inpvalue, setinpvalue] =  useState("");
+  const [result, setResult] =useState("0");
+  
+  const calculate=(e)=>{
+    setinpvalue(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <div className='inputs'>
+          <input type="text" value={inpvalue} name="input" onChange={calculate} placeholder="Type here"></input>
+          <p>{result}</p>
+        </div>
+        <div className='ac'>
+        <button onClick={()=>{setinpvalue("")
+      setResult(0)}}>AC</button>
+      </div>
+      <div className='buttons'>
+        <button onClick={()=>{setinpvalue(inpvalue+"1")}}>1</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"2")}}>2</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"3")}}>3</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"/")}}>/</button>
+      </div>
+      <div className='buttons'>
+        <button onClick={()=>{setinpvalue(inpvalue+"4")}}>4</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"5")}}>5</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"6")}}>6</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"*")}}>*</button>
+      </div>
+      <div className='buttons'>
+        <button onClick={()=>{setinpvalue(inpvalue+"7")}}>7</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"8")}}>8</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"9")}}>9</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"-")}}>-</button>
+      </div>
+      <div className='buttons'>
+        <button onClick={()=>{setinpvalue(inpvalue+"0")}}>0</button>
+        <button onClick={()=>{setinpvalue(inpvalue+".")}}>.</button>
+        <button onClick={()=>{setResult(eval(inpvalue));}}>=</button>
+        <button onClick={()=>{setinpvalue(inpvalue+"+")}}>+</button>
+      </div>
     </div>
   );
 }
